0dd48c34fbdf3873de3d2d377206acf92e3c6544
Crystal Perreira <cvperreira@gmail.com>
2014-11-14 16:23:30 -0800

    MVP

---
+++ mylist.js
@@ -0,0 +1,66 @@
+$(function (){
+  //var myHash = {}; // put in as a param
+  //$("ul").sortable();
+  var myArray = [];
+  $("#myButton").click(function(event){ 
+    event.preventDefault();
+    myArray.push([$("#myIng").val(), $("#myPer").val(), "empty"]);
+    var $myLine = $(
+      "<tr class='line'>" + 
+        "<td>" + $("#myPer").val() + "</td>" +
+        "<td>" + $("#myIng").val() + "</td>" +
+      "</tr>"
+    ).click({param1: myArray, param2: myArray.length-1}, selector);
+    console.log(myArray);
+    $("tbody").append($myLine);
+  });
+ 
+  function selector(event) {
+    $(this).addClass("selected").siblings().removeClass("selected");
+    if ($(this).hasClass("selected")) {
+      var position = event.data.param2; 
+      var newArray = emptier(event.data.param1);
+      newArray[position][2]= prompt(
+        "Enter the amount of " + newArray[position][0] + " in grams:");
+      factor = newArray[position][2]/newArray[position][1];
+      newArray = calculate(newArray, factor);
+      console.log(newArray);
+      printer(newArray, $("tbody"));
+    };
+  };
+
+  function calculate(array, factor) {
+    for (var i= 0; i<array.length; ++i){
+      if (array[i][2]=== "empty") {
+        array[i][2]=Math.round(factor*array[i][1]);
+      };  
+    };
+    return array;
+  };
+  
+  function emptier(array){
+    for (var i= 0; i<array.length; ++i){
+      array[i][2] = "empty"
+    };
+    return array;
+  };
+
+  function printer(array, old_tbody){
+    var new_tbody = document.createElement('tbody');
+    if (!$("#tableHeader").hasClass("header")) {
+      $("#tableHeader").append("<th>amount</th>"); 
+      $("#tableHeader").addClass("header");
+    };
+    for (var i= 0; i<array.length; ++i){
+      var $myLine = $(
+        "<tr class='line'>" + 
+          "<td>" + array[i][1] + "</td>" +
+          "<td>" + array[i][0] + "</td>" +
+          "<td>" + array[i][2] + "</td>" +
+        "</tr>").click({param1: array, param2: array.length-1}, selector);
+      $(new_tbody).append($myLine);      
+    };
+    $(old_tbody).replaceWith($(new_tbody));
+    //old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
+  };
+});
